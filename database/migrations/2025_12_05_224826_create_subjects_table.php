38021717db42c00c59a5d799b8c87819ba8e75ed
Dayoebe <oyetoke.ebenezer@gmail.com>
2025-12-06 00:06:51 +0100

    Added subjects table

--- database/migrations/2025_12_05_224826_create_subjects_table.php
+++ database/migrations/2025_12_05_224826_create_subjects_table.php
@@ -6,22 +6,54 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
+        // Subjects table
         Schema::create('subjects', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('short_name');
+            $table->foreignId('school_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('my_class_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->boolean('is_general')->default(false);
+            $table->softDeletes();
+            $table->timestamps();
+            $table->unique(['name', 'my_class_id']);
+        });
+
+        // Section Subject pivot table
+        Schema::create('section_subject', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('section_id')->constrained()->onDelete('cascade');
+            $table->foreignId('subject_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        // Subject User (Teacher assignment) pivot table
+        Schema::create('subject_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('subject_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+
+        // Syllabi table
+        Schema::create('syllabi', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->string('file');
+            $table->foreignId('subject_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('semester_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
+        Schema::dropIfExists('syllabi');
+        Schema::dropIfExists('subject_user');
+        Schema::dropIfExists('section_subject');
         Schema::dropIfExists('subjects');
     }
 };
